34351a526d186f707130423adde58a5b56c97690
angeldhs <angeldehoyos2000@gmail.com>
2024-10-25 20:38:45 +0200

    Se añade archivo Ventana.py

---
+++ Ventana.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 25 20:13:25 2024
+
+Interfaz gráfica para visualizar los términos de fibonacci.
+
+@author: Angel de Hoyos Sainz
+"""
+import tkinter as tk 
+
+raiz = tk.Tk()
+
+raiz.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
